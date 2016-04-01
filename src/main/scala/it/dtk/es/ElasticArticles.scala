162d459883edb3d80704740fd6e72579662c3b26
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-01 20:37:22 +0200

    minor

--- src/main/scala/it/dtk/es/ElasticArticles.scala
+++ src/main/scala/it/dtk/es/ElasticArticles.scala
@@ -2,15 +2,14 @@ package it.dtk.es
 
 import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s._
-import it.dtk.HttpDownloader
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
-import org.elasticsearch.common.io.stream
 import org.elasticsearch.common.settings.Settings
-import org.json4s._
+import org.json4s.JsonAST.JValue
+import org.json4s.{ StringInput, NoTypeHints }
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.jackson.Serialization
+import org.json4s.jackson.JsonMethods._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, Future }
 
 /**
  * Created by fabiofumarola on 16/03/16.
@@ -27,4 +26,10 @@ class ElasticArticles(hosts: String, indexPath: String, clusterName: String) {
     index exists indexPath
   }.await.isExists
 
+  def rawQuery(rawQuery: String)(implicit ex: ExecutionContext): Future[JValue] = {
+    val query = search in indexPath rawQuery rawQuery
+
+    client.execute(query)
+      .map(r => parse(r.original.toString))
+  }
 }

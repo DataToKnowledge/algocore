3a9862adcbad92829e7e480e26b790c458597af7
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-02 12:52:47 +0200

    update to search for json

--- src/main/scala/it/dtk/es/ElasticArticles.scala
+++ src/main/scala/it/dtk/es/ElasticArticles.scala
@@ -4,7 +4,7 @@ import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s._
 import org.elasticsearch.common.settings.Settings
 import org.json4s.JsonAST.JValue
-import org.json4s.{ StringInput, NoTypeHints }
+import org.json4s._
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.JsonMethods._
@@ -28,6 +28,13 @@ class ElasticArticles(hosts: String, indexPath: String, clusterName: String) {
 
   def rawQuery(rawQuery: String)(implicit ex: ExecutionContext): Future[JValue] = {
     val query = search in indexPath rawQuery rawQuery
+
+    client.execute(query)
+      .map(r => parse(r.original.toString))
+  }
+
+  def rawQuery(jsonQuery: JValue)(implicit ex: ExecutionContext): Future[JValue] = {
+    val query = search in indexPath rawQuery compact(render(jsonQuery))
 
     client.execute(query)
       .map(r => parse(r.original.toString))

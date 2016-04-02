93caedee476e19132672594f479f02c6abff9a59
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-02 16:46:23 +0200

    bump version

--- src/main/scala/it/dtk/es/ElasticArticles.scala
+++ src/main/scala/it/dtk/es/ElasticArticles.scala
@@ -9,12 +9,13 @@ import org.json4s._
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.jackson.JsonMethods._
 import org.json4s.jackson.Serialization
+import ElasticQueryConverter._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 /**
-  * Created by fabiofumarola on 16/03/16.
-  */
+ * Created by fabiofumarola on 16/03/16.
+ */
 class ElasticArticles(hosts: String, indexPath: String, clusterName: String) {
   implicit val formats = Serialization.formats(NoTypeHints) ++ JodaTimeSerializers.all
 
@@ -35,51 +36,63 @@ class ElasticArticles(hosts: String, indexPath: String, clusterName: String) {
   }
 
   def rawQuery(jsonReq: JValue)(implicit ex: ExecutionContext): Future[JValue] = {
-    val q = jsonReq \ "query"
-    val src = (jsonReq \ "_source").extractOpt[List[String]]
-    val from = (jsonReq \ "from").extractOpt[Int]
-    val size = (jsonReq \ "size").extractOpt[Int]
-    val sort = jsonReq \ "sort"
-    val aggs = jsonReq \ "aggs"
+    val parsed = ParsedQuery(jsonReq)
+    val query = search.in(indexPath).rawQuery(parsed.query)
 
-    var query = search.in(indexPath).rawQuery(compact(render(q)))
+    parsed.sources.foreach(s => query.sourceInclude(s: _*))
+    parsed.from.foreach(query.from)
+    parsed.size.foreach(query.size)
 
-    src.foreach(s => query.sourceInclude(s: _*))
-    from.foreach(query.from)
-    size.foreach(query.size)
+    val sorted = parsed.sort.map(e => fieldSort(e.field).order(e.sort))
+    query.sort(sorted: _*)
 
     client.execute(query)
       .map(r => parse(r.original.toString, false))
   }
 
-  def rawQuery2(jsonReq: JValue)(implicit ex: ExecutionContext): Future[JValue] = {
-    val q = jsonReq \ "query"
-    val src = (jsonReq \ "_source").extractOpt[List[String]]
-    val from = (jsonReq \ "from").extractOpt[Int]
-    val size = (jsonReq \ "size").extractOpt[Int]
-    val sort = jsonReq \ "sort"
-    val aggs = jsonReq \ "aggs"
-
-
-    var query = search.in(indexPath).rawQuery(compact(render(q)))
-
-    src.foreach(s => query.sourceInclude(s: _*))
-    from.foreach(query.from)
-    size.foreach(query.size)
-    query.sort(fieldSort("").order(SortOrder.valueOf("asc")))
-
-    ???
-
-    //    val r: WrapperQueryBuilder = new WrapperQueryBuilder(compact(render(jsonReq)))
-    //
-    //
-    //
-    //    val query = search in indexPath rawQuery compact(render(jsonReq))
-    //    client.execute(query)
-    //      .map(r => parse(r.original.toString, false))
-  }
-
-  def addSorts(jArray: JArray) = {
-
-  }
 }
+
+object ElasticQueryConverter {
+  implicit val formats = Serialization.formats(NoTypeHints) ++ JodaTimeSerializers.all
+
+  case class ParsedQuery(
+    query: String,
+    sources: Option[List[String]],
+    from: Option[Int],
+    size: Option[Int],
+    sort: List[SortedField]
+  )
+
+  case class SortedField(field: String, sort: SortOrder)
+
+  object ParsedQuery {
+    def apply(raw: JValue): ParsedQuery = {
+      val q = compact(render(raw \ "query"))
+      val src = (raw \ "_source").extractOpt[List[String]]
+      val from = (raw \ "from").extractOpt[Int]
+      val size = (raw \ "size").extractOpt[Int]
+
+      val fieldsToSort = SortedField(raw \ "sort")
+
+      val aggs = raw \ "aggs"
+
+      ParsedQuery(q, src, from, size, fieldsToSort)
+    }
+  }
+
+  object SortedField {
+    def apply(value: JValue): List[SortedField] = {
+      value match {
+        case JNothing => List.empty
+
+        case JArray(values) =>
+          values.map {
+            case JObject(List((field, JObject(List((_, JString(order))))))) =>
+              SortedField(field, SortOrder.valueOf(order.toUpperCase))
+          }
+      }
+    }
+  }
+
+}
+
--- src/main/scala/it/dtk/es/QueryHelper.scala
+++
@@ -1,8 +0,0 @@
-package it.dtk.es
-
-/**
-  * Created by fabiofumarola on 02/04/16.
-  */
-trait QueryHelper {
-
-}

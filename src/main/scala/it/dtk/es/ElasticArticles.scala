cc633cc9c587b844436de6fa69f565278c3c8cc7
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-16 19:14:46 +0200

    update

--- src/main/scala/it/dtk/es/ElasticArticles.scala
+++ src/main/scala/it/dtk/es/ElasticArticles.scala
@@ -11,11 +11,11 @@ import org.json4s.jackson.JsonMethods._
 import org.json4s.jackson.Serialization
 import play.api.libs.json._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 /**
-  * Created by fabiofumarola on 16/03/16.
-  */
+ * Created by fabiofumarola on 16/03/16.
+ */
 class ElasticArticles(hosts: String, searchHost: String, indexPath: String, clusterName: String) {
   implicit val formats = Serialization.formats(NoTypeHints) ++ JodaTimeSerializers.all
 
@@ -29,7 +29,7 @@ class ElasticArticles(hosts: String, searchHost: String, indexPath: String, clus
   }.await.isExists
 
   def rawQuery(data: JValue)(implicit ex: ExecutionContext): Future[JValue] = {
-    val url = s"http://$searchHost/news/articles/_search"
+    val url = s"http://$searchHost/$indexPath/_search"
     val json = Json.parse(pretty(render(data)))
     HttpDownloader.ws.url(url).post(json).map { res =>
       parse(res.body, false)
